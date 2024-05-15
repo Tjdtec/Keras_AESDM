c20d78aecad0280797d571f52ed0f1b57a2f9a6a
Tjdtec <125630225+Tjdtec@users.noreply.github.com>
2024-05-15 14:17:38 +0800

    Update feature_selection.py

--- feature_selection.py
+++ feature_selection.py
@@ -11,8 +11,6 @@ test.label = np.where(test.label == "normal", 0, 1)
 train.label = np.where(train.label == "normal", 0, 1)
 
 model = XGBClassifier()
-
-# 创建 LabelEncoder 对象
 label_encoder = LabelEncoder()
 
 # 对指定列进行编码转换
@@ -22,22 +20,12 @@ for column in columns_to_encode:
 
 X = train.iloc[:, :-1]
 y = train.iloc[:, -1]
-# 使用 SelectFromModel 进行特征选择
 selector = SelectFromModel(model)
 
 selector.fit(X, y)
 
-# 获取特征的重要性得分
 feature_importances = selector.estimator_.feature_importances_
-
-# 设置阈值，选择至少 20 个重要特征
 threshold = sorted(feature_importances, reverse=True)[20]  # 取第 20 大的重要性得分作为阈值
-
-# 获取被选择的特征的索引
 selected_indices = [i for i, importance in enumerate(feature_importances) if importance >= threshold]
-
-# 获取被选择的特征的列名
 selected_columns = X.columns[selected_indices]
-
-# 打印被选择的特征的列名
 print("Selected columns:", selected_columns)
